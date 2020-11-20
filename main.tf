eec41d0ee724422b46a04dad94e31274e91fa9d6
mibrahimmalik@gmail.com <mibrahimmalik@gmail.com>
2020-11-20 11:16:25 +0000

    initial commit 11

--- main.tf
+++ main.tf
@@ -13,18 +13,19 @@ terraform {
     workspaces {
       name = "terraform_cloud"
     }
-  }
-
-  features {}
+  }  
 }
 
-/*
 provider "azurerm" {
   features {}
   //version = ">= 2.26"
+ 
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 
 }
-*/
 
 resource "azurerm_resource_group" "rg" {
   name     = "testtf-rg"
