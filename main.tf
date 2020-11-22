3b4568631ebd49dff043fd89024987d19a1d15fb
mibrahimmalik@gmail.com <mibrahimmalik@gmail.com>
2020-11-22 18:35:19 +0000

    addded Tags

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 resource "azurerm_resource_group" "rg" {
   name     = "testtf-rg"
   location = "uksouth"
+
+    tag = {
+        environment = "dev"
+        owner = "Muhammad Ibrahim"
+    }
+
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -8,6 +14,10 @@ resource "azurerm_virtual_network" "vnet" {
     address_space = ["10.1.0.0/16"]
     resource_group_name = azurerm_resource_group.rg.name
     location = "uksouth"
+
+    tag = {
+        environment = "dev"
+    }
 
 
     subnet {
