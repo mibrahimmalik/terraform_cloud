2e450c0e374a0c0056493566ed4f9e112baf0aec
mibrahimmalik@gmail.com <mibrahimmalik@gmail.com>
2021-02-23 19:30:59 +0000

    correct auth and backend config

--- backend.tf
+++ backend.tf
@@ -1,5 +1,12 @@
 # Configure remote backend
-
+provider "azurerm" {
+  features {}
+  //version = ">= 2.26" 
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+}
 
 terraform {
   required_providers {
